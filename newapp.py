2b4dc1a0288e4f06936bcfaa0470c90203a68209
Ritesh Chougule <107393912+RiteshRC96@users.noreply.github.com>
2025-05-14 18:12:40 +0530

    Update newapp.py

--- newapp.py
+++ newapp.py
@@ -13,7 +13,7 @@ user_avatar = image_to_base64("static/user.png")
 bot_avatar = image_to_base64("static/bot.png")
 
 # Initialize bot
-groq_api_key = "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"
+groq_api_key = "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"
 bot = SmartChatBot(groq_api_key)
 
 # Page config
